8302b68716f964ad09d7dad1f1a16fd94324f978
Tamanna Murad <tamannamurad246810@gmail.com>
2022-07-17 16:40:07 +0530

    again-fixed

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -1,4 +1,4 @@
-const { default: axios } = require("axios");
+// const { default: axios } = require("axios");
 
 // const { default: axios } = require('axios');
 const result = document.querySelector('.result');
